6545101c7abc649fb80ee7e90a98da167f170146
makoto860 <93705485+makoto860@users.noreply.github.com>
2022-01-31 10:26:29 +0900

    index.rb

--- index.rb
+++ index.rb
@@ -1,51 +1,24 @@
 require "csv"
-puts "1(新規でメモを作成) 
-                          2(既存のメモを編集する) 
-                                                  3(全てのメモを表示) 4()"
-def make_memo
-  content = gets.chomp
-  memo = { "本文" => content}
-end
-
-def show_memo(memos)
-  memos.each do |memo|
-    puts memo ["本文"]
-  end
-end
-
-memos = []
-
 while true
-  
-  puts "番号選択してください。"
-  mode = gets.chomp
-  if mode == "1"
-    puts "拡張子を除いたファイル名を入力してください"
-    mode = gets.chomp
-    
-    puts "内容を入力してください。"
-    puts "完了後、Ctrl + Dを押します"
-    
-    memos.push(make_memo)
-    
-  elsif mode == "2"
-    puts "拡張子を除いたファイル名を入力してください"
-    mode = gets.chomp
-    puts "編集内容を入力する"
-    puts "完了後、Ctrl + Dを押します"
-    memos.push(make_memo)
-  
-  elsif mode == "3"
-    puts "拡張子を除いたファイル名を入力してください"
-    mode = gets.chomp
-    puts "全てのメモを表示します。"
-    show_memo(memos)
-  elsif mode == ""
-    puts ""
-    puts ""
-  else 
-    puts "エラー"
-  end
-end
+puts "1(新規でメモを作成) 2(既存のメモを編集する)"
 
+  memo_type = gets.to_s
+  if memo_type = "1"
+   puts "拡張子を除いたファイル名を入力してください"
+   memo_type = gets.to_s
+   puts "メモしたい内容を記入してください"
+   puts "完了後、Ctrl + Dを押します"
+   
+   memo_type = $stdin.read
+   CSV.open("test.csv","w") do |test|
+    test << ["#{memo_type}"]
+   end
+  elsif memo_type = "2"
+   puts "編集したいファイル名を入力してください"
+   memo_type = gets.to_s
+ 　 puts "編集内容を記入してください"
+   puts "完了後、Ctrl + Dを押します"
+   memo_type = $stdin.read
+  end
+end 
 
